72924ca596f9ae6033d5a1bcd4dad5fdd24336ec
koungsoftware <koungsoftware@hotmail.com>
2017-01-13 01:08:00 +0530

    Model : Added properties to User

--- KoungBLL/Model/UserDAL.cs
+++ KoungBLL/Model/UserDAL.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +11,36 @@ namespace KoungBLL.Model
     class UserDAL
     {
 
+        public Guid User_Id { get; set; }
 
+        public String Salutation { get; set; }
+
+        [Required(ErrorMessage = "User Name is required")]
+        [DisplayName("User Name")]
+        [StringLength(30)]
+        public String UserId { get; set; }
+
+        [Required(ErrorMessage = "First Name is required")]
+        [DisplayName("First Name")]
+        [StringLength(50)]
+        public String UserFirstName { get; set; }
+
+
+        public String UserMI { get; set; }
+
+       
+        [Required(ErrorMessage = "Last Name is required")]
+        [DisplayName("Last Name")]
+        [StringLength(50)]
+        public String UserLastName { get; set; }
+
+
+        public String EmailId { get; set; }
+
+        public String Password { get; set; }
+
+
+        public String Status { get; set; }
+    
     }
 }
